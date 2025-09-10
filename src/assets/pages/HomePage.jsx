c633d9010761f354ea2a6300ecd40d17c641f7c1
puga2 <o.kimhong168@gmail.com>
2025-09-10 15:48:50 +0700

    responsive our home page

--- src/assets/pages/HomePage.jsx
+++ src/assets/pages/HomePage.jsx
@@ -4,9 +4,9 @@ const HomePage = () => {
   return (
     <>
       <div className=" bg-[#016241]  mx-auto h-[337px]  max-w-7xl rounded-b-3xl mb-15">
-        <div className="hero-content px-25 py-20  justify-start items-start ">
+        <div className="hero-content px-10 md:px-25 py-20  justify-start items-start ">
           <div className="mx-0 ">
-            <h1 className="text-6xl font-bold text-start tracking-wide">
+            <h1 className="text-3xl md:text-6xl font-bold text-start tracking-wide">
               ANN Law Office
             </h1>
             <p className="py-6 text-lg font-normal tracking-wide">
@@ -19,13 +19,13 @@ const HomePage = () => {
       {/* feature  */}
       <div className="hero bg-base-content mx-auto max-w-7xl mb-10">
         <div className="hero-content flex-col-reverse lg:flex-row-reverse justify-center items-start p-4 md:p-8 lg:p-12 w-full">
-          <img src={home} className="max-w-xl lg:max-w-sm shadow-2xl mx-auto" />
+          <img src={home} className="sm:max-w-[50px]  md:max-w-xl  shadow-2xl mx-auto" />
           <div>
-            <h1 className="text-4xl font-bold text-[#016241] tracking-wide">
+            <h1 className="text-3xl md:text-4xl font-bold text-[#016241] tracking-wide">
               ANN Law Office is a <br /> leading full-service law firm <br />{" "}
               operating in Cambodia.
             </h1>
-            <hr className="w-[480px] border-t-8 border-[#016241] my-4" />
+            <hr className="w-[280px] md:w-[480px] border-t-8 border-[#016241] my-4" />
             <p className="py-6 tracking-wider text-black text-lg leading-loose">
               ANN is a term derived from Sanskrit meaning limitless, infinite.
               This concept is deeply rooted in Khmer culture. ANANT Law Firm was
